e01c45069cfccf0d24ddd49723c0a7272110e1fd
Cody Peter <crpeter@iastate.edu>
2018-09-18 17:12:39 -0500

    added list item page

---
+++ Frontend/app/src/main/java/peddler/coms309/petercody/peddler/ListItemActivity.java
@@ -0,0 +1,14 @@
+package peddler.coms309.petercody.peddler;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ListItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item);
+    }
+
+}
--- Frontend/app/src/main/java/peddler/coms309/petercody/peddler/MainActivity.java
+++ Frontend/app/src/main/java/peddler/coms309/petercody/peddler/MainActivity.java
@@ -1,13 +1,28 @@
 package peddler.coms309.petercody.peddler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button listItem = findViewById(R.id.list_item);
+        listItem.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v)
+    {
+        if (v.getId() == R.id.list_item) {
+            startActivity(new Intent(MainActivity.this, ListItemActivity.class));
+        }
+        return;
     }
 }
