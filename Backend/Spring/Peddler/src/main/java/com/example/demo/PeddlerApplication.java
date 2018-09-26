6651225a41bccdc1b1379e62eaab7990b32bd4c6
Sam <scstanek@iastate.edu>
2018-09-26 13:07:47 -0500

    Spring Project

---
+++ Backend/Spring/Peddler/src/main/java/com/example/demo/PeddlerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeddlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeddlerApplication.class, args);
+	}
+}
