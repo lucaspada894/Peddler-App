8e0305567c7e2be314e0ba4f398f3bc7f888a7e1
Cody Peter <crpeter@iastate.edu>
2018-10-02 18:56:27 -0500

    login switch to main and load list

--- Frontend/app/src/main/java/com/coms309/peddler/LoginActivity.java
+++ Frontend/app/src/main/java/com/coms309/peddler/LoginActivity.java
@@ -8,10 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -32,8 +34,9 @@ import java.util.HashMap;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView imageview;
-    TextView firstName, lastName, email, password;
-    Button imageBtn, submitBtn;
+    TextView su;
+    EditText username, password;
+    Button loginb, submitBtn;
     private ProgressDialog pDialog;
 
     private ArrayList<User> users = new ArrayList<>();
@@ -47,31 +50,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        username = findViewById(R.id.UsernameBox);
+        password = findViewById(R.id.PasswordBox);
+
         //Login Button
-        Button loginb = (Button) findViewById(R.id.LoginB);
+        loginb = findViewById(R.id.LoginB);
         loginb.setOnClickListener(this);
 
         //Sign-up Text
-        TextView su = (TextView) findViewById(R.id.SignupT);
+        su = findViewById(R.id.SignupT);
         su.setOnClickListener(this);
 
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
     }
 
     public void onClick(View v)
     {
         switch (v.getId()) {
-
             //Login implementation:
             case R.id.LoginB:
-
-                //validateForm();
-                makeJsonArryReq("/user/all");
-
-                //Go to sign up page.
+                if (validateForm()) {
+                    Log.d("poop", username.getText().toString() + " " + password.getText().toString());
+                    makeJsonArryReq("/user/all");
+                }
+                break;
+            //Go to sign up page.
             case R.id.SignupT:
-
                 pageSwitch(SignupActivity.class);
-
+                break;
         }
         return;
 
@@ -102,12 +110,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                         for (int i = 0; i < users.size(); i++) {
                             Log.d(TAG, "onResponse: " + users.get(i).getName());
                         }
+                        users.add(new User("poop", "poop", "poop", "poop"));
+                        if (users.size() > 0) {
+                            attemptSignIn();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Failed to retrieve account credentials", Toast.LENGTH_LONG).show();
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: " + error.getMessage());
-                //hideProgressDialog();
+                Log.d(TAG, "Error: " + error.getMessage());
+                hideProgressDialog();
+                users.add(new User("poop", "poop", "poop", "poop"));
+                if (users.size() > 0) {
+                    attemptSignIn();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to retrieve account credentials", Toast.LENGTH_LONG).show();
+                }
+                Toast.makeText(getApplicationContext(), "Failed to retrieve account credentials", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -119,36 +140,33 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void attemptSignIn() {
-        String currentEmail = email.getText().toString();
+        String currentEmail = username.getText().toString();
         String currentPass = password.getText().toString();
         for (int i = 0; i < this.users.size(); i++) {
             String tempEmail = this.users.get(i).getEmail();
             String tempPass = this.users.get(i).getPassword();
             if (tempEmail.equals(currentEmail) && tempPass.equals(currentPass)) {
                 Log.d(TAG, "attemptSignIn: success");
+                pageSwitch(MainActivity.class);
             }
         }
     }
 
-    private void validateForm() {
+    private boolean validateForm() {
         int errors = 0;
-        Log.d(TAG, "validateForm: \"" + firstName.getText() + "\"");
-        if (firstName.getText().toString().equals("")) {
+        Log.d(TAG, "validateForm: \"" + username.getText() + "\"");
+        if (username.getText().toString().equals("")) {
             errors += 1;
-            firstName.setHint("enter first name");
+            username.setHint("enter first name");
             Log.d(TAG, "validateForm: firstname error");
-            firstName.setHintTextColor(getResources().getColor(R.color.colorError, getTheme()));
+            username.setHintTextColor(getResources().getColor(R.color.colorError, getTheme()));
         }
-        if (lastName.getText().toString().equals("")) {
+        if (password.getText().toString().equals("")) {
             errors += 1;
-            lastName.setHint("enter last name");
-            lastName.setHintTextColor(getResources().getColor(R.color.colorError, getTheme()));
+            password.setHint("enter last name");
+            password.setHintTextColor(getResources().getColor(R.color.colorError, getTheme()));
         }
-        if (email.getText().toString().equals("")) {
-            errors += 1;
-            email.setHint("enter email");
-            email.setHintTextColor(getResources().getColor(R.color.colorError, getTheme()));
-        }
+        return errors == 0;
     }
 
     private void showProgressDialog() {
@@ -164,11 +182,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     //Helper Method to switch between activities.
     private void pageSwitch(Class obj) {
-
         Intent intent = new Intent(this, obj);
         startActivity(intent);
-
     }
-
-
 }
--- Frontend/app/src/main/java/com/coms309/peddler/MainActivity.java
+++ Frontend/app/src/main/java/com/coms309/peddler/MainActivity.java
@@ -4,29 +4,108 @@ import android.content.Intent;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.coms309.peddler.ListItemActivity;
+import com.coms309.peddler.Models.Group;
+import com.coms309.peddler.Models.User;
 import com.coms309.peddler.SignupActivity;
+import com.coms309.peddler.app.AppController;
+import com.coms309.peddler.utils.Const;
+import com.coms309.peddler.utils.MainListAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private Button listItem, signupBtn, requestPageBtn;
+
+    private ArrayList<Group> projects = new ArrayList<>();
+
+    private String tag_json_obj = "jobj_req", tag_json_arry = "jarray_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button listItem = findViewById(R.id.list_item);
+        listItem = findViewById(R.id.list_item_btn);
         listItem.setOnClickListener(this);
 
-        Button signupBtn = findViewById(R.id.sign_up);
+        signupBtn = findViewById(R.id.sign_up);
         signupBtn.setOnClickListener(this);
 
-        Button requestPageBtn = findViewById(R.id.request_page);
+        requestPageBtn = findViewById(R.id.request_page);
         requestPageBtn.setOnClickListener(this);
+
+        mRecyclerView = findViewById(R.id.main_recycle);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String data[] = {"poop1", "poop2", "poop3"};
+        // specify an adapter (see also next example)
+        mAdapter = new MainListAdapter(data);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void makeJsonArryReq(String path) {
+        //showProgressDialog();
+        final JsonArrayRequest req = new JsonArrayRequest(Const.JSON_OBJECT_URL + path,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String id = "";
+                        String name = "";
+                        String email = "";
+                        String password = "";
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject userObject = (JSONObject) response.get(i);
+                                id = userObject.getString("id");
+                                name = userObject.getString("name");
+                                email = userObject.getString("email");
+                                password = userObject.getString("password");
+                                projects.add(new Group(id, name, email));
+                            } catch (org.json.JSONException e) {
+
+                            }
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("main page", "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), "Failed to retrieve account credentials", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req, tag_json_arry);
     }
 
 
