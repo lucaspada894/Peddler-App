6bf37f2106e95e5882586ab6a8329d0f00ca15e4
Sam <scstanek@iastate.edu>
2018-11-05 21:46:10 -0600

    added images

---
+++ Backend/Spring/Peddler/src/main/java/org/springframework/samples/peddler/images/FileController.java
@@ -0,0 +1,103 @@
+package org.springframework.samples.peddler.images;
+
+
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+public class FileController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    private FileStorageService fileStorageService;
+    
+   /* @RequestMapping(path= "/uploadFile", method = RequestMethod.POST)
+    public UploadFileResponse uploadFile(@RequestParam("file") String file) {
+    	byte[] imageByte=Base64.decodeBase64(file);
+    	
+    	
+
+    	String name = "file.png";
+    	String originalFileName = "file.png";
+    	String contentType = "file/png";
+    	byte[] content = imageByte;
+
+    	MultipartFile result = new MockMultipartFile(name, originalFileName, contentType, content);
+        String fileName = fileStorageService.storeFile(result);
+
+        
+        
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new UploadFileResponse(fileName, fileDownloadUri,
+                contentType, content.length);
+    }
+
+    
+    
+    /*@PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.asList(files)
+                .stream()
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }*/
+
+    
+    
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public @ResponseBody ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        // Load file as Resource
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+   
+    
+}
