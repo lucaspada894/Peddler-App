0bb022770b4f9df0bf7e5c5784134bbf026f8995
Cody Peter <crpeter@iastate.edu>
2018-11-06 21:17:34 -0600

    adfs

--- Frontend/app/src/main/java/com/coms309/peddler/Messages/MessagePage.java
+++ Frontend/app/src/main/java/com/coms309/peddler/Messages/MessagePage.java
@@ -25,6 +25,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
+import com.coms309.peddler.Models.Message;
 import com.coms309.peddler.Models.User;
 import com.coms309.peddler.R;
 import com.coms309.peddler.app.AppController;
@@ -36,7 +37,9 @@ import static com.coms309.peddler.utils.Const.WEBSOCKET_URL;
 public class MessagePage extends AppCompatActivity implements View.OnClickListener {
 
     //Fields
-    static ArrayList<String> messages = new ArrayList<>();
+    static ArrayList<Message> messagesFromUser = new ArrayList<>();
+    static ArrayList<Message> messagesFromRecipient = new ArrayList<>();
+
     static String currentUserID;
     static String recipientUserID;
 
@@ -58,15 +61,15 @@ public class MessagePage extends AppCompatActivity implements View.OnClickListen
         messageField = findViewById(R.id.enterText);
 
         Bundle extras = getIntent().getExtras();
-        if(extras !=null) {
+        if (extras !=null) {
             this.setTitle(extras.getString("USER_NAME"));
             this.currentUserID = extras.getString("USER_ID");
             this.recipientUserID = extras.getString("REC_ID");
         }
 
         Log.d("message page:", "cur user id: [" + currentUserID + "], rec id: [" + recipientUserID + "]");
-        makeJsonArryReq("/message/getBySender?creatorId=" + currentUserID);
-        makeJsonArryReq("/message/getBySender?creatorId=" + recipientUserID);
+        makeJsonArryReq("/message/getBySender?creatorId=" + currentUserID, true);
+        makeJsonArryReq("/message/getBySender?creatorId=" + recipientUserID, false);
         connect_to_socket();
     }
 
@@ -114,17 +117,31 @@ public class MessagePage extends AppCompatActivity implements View.OnClickListen
         }
     }
 
-    private void makeJsonArryReq(String path) {
+    private void makeJsonArryReq(String path, final boolean currentUser) {
         //showProgressDialog();
         final JsonArrayRequest req = new JsonArrayRequest(Const.JSON_OBJECT_URL_SERVER + path,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
                         String id = "";
+                        if (currentUser) {
+                            messagesFromUser.clear();
+                        } else {
+                            messagesFromRecipient.clear();
+                        }
                         for (int i = 0; i < response.length(); i++) {
                             try {
-                                JSONObject userObject = (JSONObject) response.get(i);
-                                Log.d("response msg:", userObject.toString());
+                                JSONObject msgObject = (JSONObject) response.get(i);
+                                Log.d("response msg:", msgObject.toString());
+                                if (currentUser) {
+                                    messagesFromUser.add(new Message(msgObject.getString("creatorId"),
+                                                                     msgObject.getString("recipientId"),
+                                                                     msgObject.getString("message")));
+                                } else {
+                                    messagesFromRecipient.add(new Message(msgObject.getString("creatorId"),
+                                                                          msgObject.getString("recipientId"),
+                                                                          msgObject.getString("message")));
+                                }
                             } catch (org.json.JSONException e) {
 
                             }
@@ -134,11 +151,65 @@ public class MessagePage extends AppCompatActivity implements View.OnClickListen
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.d("message page:", "Error: " + error.getMessage());
-                Toast.makeText(getApplicationContext(), "Failed to retrieve any friends", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Failed to retrieve any messages", Toast.LENGTH_SHORT).show();
             }
         });
         AppController.getInstance().addToRequestQueue(req, tag_json_arry);
         // Cancelling request
         // ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_arry);
+    }
+
+    void filterMessages(ArrayList<Message> toRecMsg, ArrayList<Message> fromRecMsg) {
+        for (int i = 0; i < toRecMsg.size(); i++) {
+            if (!toRecMsg.get(i).getRecId().equals(recipientUserID)) {
+                toRecMsg.remove(i);
+                i = 0;
+            }
+        }
+        for (int i = 0; i < fromRecMsg.size(); i++) {
+            if (!fromRecMsg.get(i).getRecId().equals(currentUserID)) {
+                fromRecMsg.remove(i);
+                i = 0;
+            }
+        }
+    }
+
+    void connectToWebsocket() {
+        Draft[] drafts = {new Draft_6455()};
+        String w = WEBSOCKET_URL;
+        w += currentUserID;
+        Log.d("websocket url", w);
+        try {
+            Log.d("Socket:", "Trying socket");
+            cc = new WebSocketClient(new URI(w), (Draft) drafts[0]) {
+                @Override
+                public void onMessage(String message) {
+                    Log.d("", "run() returned: " + message);
+                    if (message.toString().equals("SUCCESS")) {
+                        makeJsonArryReq("/message/getBySender?creatorId=" + currentUserID, true);
+                        makeJsonArryReq("/message/getBySender?creatorId=" + recipientUserID, false);
+                    }
+                }
+
+                @Override
+                public void onOpen(ServerHandshake handshake) {
+                    Log.d("OPEN", "run() returned: " + "is connecting");
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    Log.d("CLOSE", "onClose() returned: " + reason);
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    Log.d("Exception:", e.toString());
+                }
+            };
+        } catch (URISyntaxException e) {
+            Log.d("Exception:", e.getMessage().toString());
+            e.printStackTrace();
+        }
+        cc.connect();
     }
 }
